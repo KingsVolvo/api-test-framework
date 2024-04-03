0c62a0dc9317a677c316b65b412720f4134c6ff7
KingsVolvo <qinqin.huang@volvocars.com>
2024-04-03 14:31:34 +0800

    check response body

--- src/api_test_case.py
+++ src/api_test_case.py
@@ -2,7 +2,7 @@ import unittest
 from api_test_model import ApiTestModel
 from utils import *
 from log_conf import Logger
-import HTMLTestRunner 
+import HTMLTestRunner
 import requests
 
 
@@ -11,15 +11,14 @@ def add(a, b):
 
 
 class ApiTestCase(unittest.TestCase):
-    
-    def set_apiurl_testmodel(self, url:str, testmodel: ApiTestModel):
+    def set_apiurl_testmodel(self, url: str, testmodel: ApiTestModel):
         self.api_url = url
         self.api_test_model = testmodel
-
 
     def test_api_test_model(self):
         api_test_url = url_slicing(self.api_url, self.api_test_model.url)
         Logger.logr.info("url:{}".format(api_test_url))
+        
         # 发送 HTTP 请求
         response = requests.request(
             self.api_test_model.method,
@@ -29,11 +28,19 @@ class ApiTestCase(unittest.TestCase):
         self.assertEqual(
             response.status_code, self.api_test_model.expected_response.get("code")
         )
-    
+        
+        
+        # 如果预期返回值中有data字段，判断返回值是否相等
+        if self.api_test_model.expected_response.get("code") == 200:
+            if self.api_test_model.expected_response.get("data"):
+                self.assertEqual(
+                    response.json(), self.api_test_model.expected_response.get("data")
+                )
+       
+
     def test_add(self):
         result = add(5, 3)
         self.assertEqual(result, 8)
-
 
     def runTest(self):
         self.test_api_test_model()
--- src/entrypoint.py
+++ src/entrypoint.py
@@ -7,14 +7,14 @@ import unittest
 import os
 import glob
 
-dir='/app/api-test/'
+dir = "/app/api-test/"
 
 if __name__ == "__main__":
-    
-    #unittest.TextTestRunner().run(api_test_suite)
-    runner = HTMLTestRunner(output='test-reports')
 
-    for file_path in glob.glob(os.path.join(dir,"*.test")):
+    # unittest.TextTestRunner().run(api_test_suite)
+    runner = HTMLTestRunner(output="test-reports")
+
+    for file_path in glob.glob(os.path.join(dir, "*.test")):
         filename = os.path.splitext(os.path.basename(file_path))[0]
         lines = filter_comments(file_path=file_path)
         Logger.logr.info(lines)
@@ -22,14 +22,11 @@ if __name__ == "__main__":
         Logger.logr.debug("test_cases:{}".format(api_test_models))
 
         api_test_suite = ApiTestSuite()
-   
+
         for model in api_test_models:
             api_test_case = ApiTestCase()
-            api_test_case.set_apiurl_testmodel(url,model)
+            api_test_case.set_apiurl_testmodel(url, model)
             api_test_suite.addTest(api_test_case)
- 
-    
-        #runner = CustomHTMLTestRunner(output='test-reports')
-        runner.run(api_test_suite)
 
-    
+        # runner = CustomHTMLTestRunner(output='test-reports')
+        runner.run(api_test_suite)
